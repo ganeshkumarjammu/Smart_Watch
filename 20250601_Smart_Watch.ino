51462141e7753afbc7cac85ebb0d5e7ba03fc804
Ganesh Kumar J <jammuganeshkumar@gmail.com>
2025-06-01 01:48:22 +0530

    v1.0

---
+++ 20250601_Smart_Watch.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#include "Adafruit_SSD1306.h"
+#define OLED_RESET -1
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
+PulseOximeter pox;
+void onBeatDetected() {
+  Serial.println("Beat detected!");
+}
+void setup() {
+  Serial.begin(115200);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  if (!pox.begin()) {
+    Serial.println("Failed to initialize pulse oximeter");
+    while (1)
+      ;
+  }
+  pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+void loop() {
+  pox.update();
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("HR:");
+  display.print(pox.getHeartRate());
+  display.print(" bpm");
+  display.setCursor(0, 20);
+  display.print("SpO2:");
+  display.print(pox.getSpO2());
+  display.print(" %");
+  display.display();
+  delay(1000);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Library References :
+https://github.com/oxullo/Arduino-MAX30100/tree/master
+
+[https://github.com/kontakt/MAX30100](https://github.com/kontakt/MAX30100)
